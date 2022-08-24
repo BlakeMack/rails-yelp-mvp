ea87aafafed007aa8fa83124441c66fa1b2b06d7
Blake McKenzie <blakemckenzie365@gmail.com>
2022-08-24 14:56:08 +0100

    restaurant routes made, Restaurants controller made

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "adding restaurants"
+10.times do
+  resto = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.leading_zero_number(digits: 10), category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  puts resto.name
+end
